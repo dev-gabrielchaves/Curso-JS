1e607674f64b37426f985eb090a98a8e40fa6f0b
dev-gabrielchaves <dev.gchaves@gmail.com>
2024-01-01 14:00:06 -0300

    Aula 11
    Learned how to use conditionals in JS

---
+++ Aula_11/ex_11.js
@@ -0,0 +1,11 @@
+var speed = 55
+
+// You must use "console.log" to show things on the terminal 
+console.log(`Your speed is currently ${speed}Km/h`)
+
+// if and else structure in JavaScript
+if (speed > 60) {
+    console.log('You are riding to fast, slow down!')
+} else {
+    console.log('Your speed is under the limit, enjoy your trip!')
+}
